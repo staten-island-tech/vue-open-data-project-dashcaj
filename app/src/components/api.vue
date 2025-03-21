f268ebf603b59ee6f52807392808fdebc8c04fec
dashcaj <158211825+dashcaj@users.noreply.github.com>
2025-03-21 11:01:41 -0400

    wdwdwddwdwdwdwwdw

---
+++ app/src/components/api.vue
@@ -0,0 +1,16 @@
+<script>
+const api = "https://data.cityofnewyork.us/resource/hjae-yuav.json"
+async function fetchData(api) {
+  try {
+    const response = await fetch(api);
+    const data = await response.json();
+    
+
+    console.log(data);
+  }
+  catch (error) {
+    console.log(error);
+  }
+}
+
+</script>
--- app/src/views/bkView.vue
+++ app/src/views/bkView.vue
@@ -1,52 +1,7 @@
 <template>
-  <div>
-    <PieChart :data="chartData" :options="chartOptions" />
-  </div>
+
 </template>
 
 <script>
-import { Pie } from 'vue-chartjs'
-import { Chart as ChartJS, Title, Tooltip, Legend, ArcElement, CategoryScale, LinearScale } from 'chart.js'
 
-// Register the necessary components with Chart.js
-ChartJS.register(Title, Tooltip, Legend, ArcElement, CategoryScale, LinearScale)
-
-export default {
-  components: {
-    PieChart: Pie
-  },
-  data() {
-    return {
-      // Chart data
-      chartData: {
-        labels: ['Red', 'Blue', 'Green'],  // Labels for each slice of the pie
-        datasets: [
-          {
-            label: 'My Pie Chart',
-            data: [1, 2, 3],  // Values for each slice
-            backgroundColor: ['red', 'blue', 'green'],  // Colors for each slice
-            hoverOffset: 4
-          }
-        ]
-      },
-      // Chart options
-      chartOptions: {
-        responsive: true,
-        maintainAspectRatio: false,
-        plugins: {
-          legend: {
-            position: 'top'
-          },
-          tooltip: {
-            callbacks: {
-              label: (tooltipItem) => {
-                return `${tooltipItem.label}: ${tooltipItem.raw} units`
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-}
 </script>
